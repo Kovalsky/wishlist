e099f9b3350e4ccb03e49fb8d26b8c407a132383
Bogdan Bondar <bogdan.bondar5@gmail.com>
2013-01-23 12:46:04 +0200

    modified landing controller

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,16 +2,9 @@ class LandingController < ApplicationController
   def login
     @title = "Welcome"
 	  auth_hash = request.env['omniauth.auth']
-=begin
-    app = VK::Application.new( access_token: auth_hash.credentials.token )
-    friends_hashes = app.friends.get( fields: 'uid, first_name, last_name, bdate, photo_max' )
-    user_hash = app.users.get( uids: auth_hash.uid, fields: 'photo_max' ).first
-=begin
-=end
 	  @app = VkontakteApi::Client.new( auth_hash.credentials.token )
 	  friends_hashes = @app.friends.get( fields: 'uid, first_name, last_name, bdate, photo_max' )
 	  user_hash = @app.users.get( uids: auth_hash.uid, fields: 'photo_max' ).first
-#=end
 	  @person = Person.find_for_vkontakte_oauth( auth_hash, friends_hashes, user_hash ) 
 
     if @person.persisted?
